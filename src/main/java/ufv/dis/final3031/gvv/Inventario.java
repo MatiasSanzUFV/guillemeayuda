bbf6505f883e90dc0b928efd864f35abad9b12c4
MatiGol Sanz <mati98sanz@hotmail.com>
2020-06-30 16:10:08 +0200

    first commit

--- src/main/java/ufv/dis/final3031/gvv/Inventario.java
+++ src/main/java/ufv/dis/final3031/gvv/Inventario.java
@@ -1,5 +1,20 @@
 package ufv.dis.final3031.gvv;
 
-public class Inventario {
+import java.util.ArrayList;
 
+public class Inventario {
+	
+	private  ArrayList<Producto> productos;
+	public Inventario()
+    {
+		productos = new ArrayList<Producto>();
+    }
+	public void addProducto(Producto p)
+    {
+        productos.add(p);
+    }
+	public void removeProducto(Producto p)
+    {
+        productos.remove(p);
+    }
 }
